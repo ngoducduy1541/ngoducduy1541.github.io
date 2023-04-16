5dbc1997b0ec9db20d7091b233310bd69a795464
ADMIN <tramdactrieu@gmail.com>
2023-04-16 18:31:05 +0700

    render simple data

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $.get("data.tsv", function(data) {
+      var rows = data.split("\n");
+      var table = $("#data-table tbody");
+      
+      for (var i = 1; i < rows.length; i++) {
+        var row = rows[i].split("\t");
+        var tr = $("<tr>");
+  
+        tr.append($("<td>").text('' + Date()));
+        tr.append($("<td>").text(row[0]));
+        tr.append($("<td>").text(row[2]));
+        tr.append($("<td>").html('<a href="' + '#' + '">' + '#' + '</a>'));
+        tr.append($("<td>").html('<img src="' + row[1] + '" width="100" height="100">'));
+        tr.append($("<td>").text('1'));
+  
+        table.append(tr);
+      }
+    });
+  });
+  
